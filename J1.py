6e977ee12a9598f2ec3c626521f01ace2af24814
YajatMittal <59126036+YajatMittal@users.noreply.github.com>
2020-03-03 15:41:10 -0500

    Update J1.py
    J1 solution

--- J1.py
+++ J1.py
@@ -1,15 +1,18 @@
-
+#Banana scoring
 def banana_points():
     three_pointers = bananas * 3
     field_goals = bananas  * 2
     free_throws = bananas * 1
+    return  free_throws + field_goals +  three_pointers
 
+#Apple scoring
 def apple_points():
     three_pointers = apples  * 3
     field_goals = apples * 2
     free_throws = apples * 1
-    
+    return free_throws + field_goals +  three_pointers 
 
+#Checking which team won
 if banana_points() > apple_points():
    print('A') 
 
@@ -18,10 +21,3 @@ elif banana_points() < apple_points():
 
 elif banana_points() == apple_points():
    print('T') 
-
-
-
-
-
-
-
