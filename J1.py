0fa18160ef5641b6f3bdc80132ccb218e844de4e
YajatMittal <59126036+YajatMittal@users.noreply.github.com>
2020-03-03 15:40:00 -0500

    Add files via upload
    J1 solution

---
+++ J1.py
@@ -0,0 +1,27 @@
+
+def banana_points():
+    three_pointers = bananas * 3
+    field_goals = bananas  * 2
+    free_throws = bananas * 1
+
+def apple_points():
+    three_pointers = apples  * 3
+    field_goals = apples * 2
+    free_throws = apples * 1
+    
+
+if banana_points() > apple_points():
+   print('A') 
+
+elif banana_points() < apple_points():
+   print('B') 
+
+elif banana_points() == apple_points():
+   print('T') 
+
+
+
+
+
+
+
